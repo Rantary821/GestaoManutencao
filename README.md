5e52dbda0733128471f6c5bb7f712455dfada326
João Souza <60327030+Rantary821@users.noreply.github.com>
2024-02-26 14:36:12 +0000

    Projeto, Login, Cadastro

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GestaoManutencao
 Projeto para gerir empresas de O&amp;M, Gerenciar, OS's, Demandas. Gerenciamento de Desligamentos, Relatorios, controle de usuario, controle de estoque, controle de funcionarios, gestão de performance.
+
+
+[.NET]
+[MongoDB]
+[React]
+[FireBase]
+[JS]
--- manutencao_client/src/App.js
+++ manutencao_client/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import CadastroUsuario from './CadastroUsuario'; // Certifique-se de que o caminho está correto
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [exibirCadastro, setExibirCadastro] = useState(false);
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/Login', {
+        Nome: username,
+        Senha: password,
+      });
+
+      setToken(response.data.Token);
+      setErrorMessage(''); 
+      setSuccessMessage('Login bem-sucedido!');
+    } catch (error) {
+      console.error('Erro ao fazer login:', error.message);
+      setErrorMessage('Credenciais inválidas'); // Define a mensagem de erro.
+      setSuccessMessage(''); // Limpa a mensagem de sucesso em caso de falha.
+    }
+  };
+
+  const handleCadastroSucesso = () => {
+    setExibirCadastro(false); // Após o cadastro bem-sucedido, volta para o formulário de login
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React API Client</h1>
+      <div>
+        <label>Nome de Usuário:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Senha:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={() => setExibirCadastro(true)}>Cadastrar</button>
+      </div>
+      {errorMessage && (
+        <div style={{ color: 'red', marginTop: '10px' }}>
+          <p>{errorMessage}</p>
+        </div>
+      )}
+      {successMessage && (
+        <div style={{ color: 'green', marginTop: '10px' }}>
+          <p>{successMessage}</p>
+        </div>
+      )}
+      {token && (
+        <div>
+          <h2>Token:</h2>
+          <p>{token}</p>
+        </div>
+      )}
+      {exibirCadastro && (
+        <CadastroUsuario onCadastroSucesso={handleCadastroSucesso} />
+      )}
     </div>
   );
 }
